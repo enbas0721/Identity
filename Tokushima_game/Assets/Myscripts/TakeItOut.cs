b2a1d0fda7497a2694393dc584a2f84b35048073
Kento Maruyama <chicken.72177@gmail.com>
2020-09-04 04:59:02 +0900

    This is first commit

---
+++ Tokushima_game/Assets/Myscripts/TakeItOut.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TakeItOut : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetObj;
+
+    [SerializeField]
+    private float changeDis;
+
+    [SerializeField]
+    private GameObject changedObj;
+
+    [SerializeField]
+    private TextMeshProUGUI distanceUI;
+
+    [SerializeField]
+    private ParticleSystem particle;
+
+    [SerializeField]
+    private AudioSource changeSound;
+
+    private bool iscalledonce = false;
+
+    private float colliderOffset;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        colliderOffset = GetComponent<CapsuleCollider>().radius + targetObj.GetComponent<CapsuleCollider>().radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(transform.position, targetObj.transform.position) - colliderOffset;
+
+        if(distanceUI != null)
+        {
+            distanceUI.text = distance.ToString("0.00m");
+        }
+        else
+        {
+            Debug.Log(distance.ToString("0.00m"));
+        }
+
+        if(distance > changeDis)
+        {
+            targetObj.SetActive(false);
+            changedObj.SetActive(true);
+            if (!iscalledonce)
+            {
+                iscalledonce = true;
+                particle.Play();
+                changeSound.Play();
+            }
+        }
+    }
+}
---
+++ Tokushima_game/Assets/Scripts/Generate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Generate : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // プレハブを取得
+        GameObject prefab = (GameObject)Resources.Load("Kuwa_tanuki");
+        // プレハブからインスタンスを生成
+        Instantiate(prefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
